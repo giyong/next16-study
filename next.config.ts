eb7b72142315e81d3b7fa8be88abf7a450548f57
yong <giyong84@gmail.com>
2026-02-11 14:37:55 +0900

    add

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // GitHub Pages는 이미지 최적화 서버 없음
   },
+  basePath: "/next16-study",
+  assetPrefix: "/next16-study/",
 };
 
 export default nextConfig;
